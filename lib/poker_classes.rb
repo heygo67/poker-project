b7d3d1fbd3513b1700e9c4d01869d704f58318f8
McKenna Noakes <mckenna.noakes@outlook.com>
2024-03-25 13:40:02 -0500

    added classes lib

---
+++ lib/poker_classes.rb
@@ -0,0 +1,23 @@
+class Card
+    #something
+end
+
+class Deck
+    #something
+end
+
+class Hand
+    #something
+end
+
+class Player
+    #something
+end
+
+class Game
+    #something
+end
+
+class Pot
+    #something
+end
