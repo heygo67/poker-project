fc9999fa932bdb2c49d38b806f400025efdc0024
McKenna Noakes <mckenna.noakes@outlook.com>
2024-03-26 11:56:18 -0500

    dealing cards is functional

--- lib/poker_classes.rb
+++ lib/poker_classes.rb
@@ -1,20 +1,34 @@
 class Card
-    def self.identify(suit, value)
-        @suit = suit
-        @value = value
-        sprintf "%s, %s", @suit, @value
+    def self.identify(deck, index)
+        card = []
+        suit = deck[index][0]
+        value = deck[index][1]
+        card << suit
+        card << value
+        return card
     end
 end
 
 class Deck
-    def self.shuffle_deck(type_suits, type_values)
+    def self.generate_deck(type_suits, type_values)
         deck = []
         type_suits.each do |suit|
             type_values.each do |value|
                 deck << [suit, value]
             end
         end
+        return deck
+    end
+
+    def self.shuffle_deck(deck)
         return deck.shuffle
+    end
+
+    def self.deal_card(deck, player, amount)
+        for index in [0, amount - 1]
+            card = Card.identify(deck, index)
+            return card
+        end
     end
 
     
@@ -40,4 +54,8 @@ num_cards_in_deck = 52
 type_suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 type_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
-deck = Deck.shuffle_deck(type_suits, type_values).inspect
+deck = Deck.generate_deck(type_suits, type_values)
+shuffled_deck = Deck.shuffle_deck(deck)
+player = nil
+card = Deck.deal_card(shuffled_deck, player, 5)
+print("Player receives ", card[1], " of ", card[0])
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -1,9 +1,11 @@
 require 'poker_classes'
+shuffled_deck = [["Diamonds", "Ace"], ["Spades", "Five"], ["Diamonds", "King"], ["Hearts", "Eight"], ["Diamonds", "Three"], ["Spades", "Six"], ["Hearts", "Jack"], ["Clubs", "Queen"], ["Spades", "Nine"], ["Spades", "Queen"], ["Hearts", "Nine"], ["Spades", "Three"], ["Hearts", "Queen"], ["Diamonds", "Four"], ["Spades", "Four"], ["Hearts", "Ace"], ["Clubs", "Four"], ["Spades", "Eight"], ["Diamonds", "Nine"], ["Hearts", "Ten"], ["Hearts", "Three"], ["Clubs", "Two"], ["Clubs", "Five"], ["Spades", "Seven"], ["Spades", "Two"], ["Clubs", "Seven"], ["Hearts", "Six"], ["Clubs", "Ten"], ["Diamonds", "Six"], ["Clubs", "Nine"], ["Clubs", "Jack"], ["Diamonds", "Queen"], ["Hearts", "Four"], ["Diamonds", "Seven"], ["Clubs", "King"], ["Hearts", "Seven"], ["Hearts", "Five"], ["Diamonds", "Jack"], ["Diamonds", "Five"], ["Hearts", "Two"], ["Spades", "Ace"], ["Diamonds", "Eight"], ["Clubs", "Eight"], ["Clubs", "Ace"], ["Clubs", "Six"], ["Spades", "King"], ["Spades", "Jack"], ["Spades", "Ten"], ["Hearts", "King"], ["Clubs", "Three"], ["Diamonds", "Two"], ["Diamonds", "Ten"]]
 
 RSpec.describe Card do
+    
     describe ".identify" do
-        it "correctly sets suit and value" do
-            expect(Card.identify("Hearts", "7")).to eq("Hearts, 7")
+        it "correctly identifies a card in a deck given index" do
+            expect(Card.identify(shuffled_deck, 2)).to eq("Diamonds, King")
         end
     end
 end
