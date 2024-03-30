466dcec4bbbbcaba7ebea98ffee4882ed0cf6475
McKenna Noakes <mckenna.noakes@outlook.com>
2024-03-29 20:29:11 -0500

    refactoring and hand updates

--- lib/poker_classes.rb
+++ lib/poker_classes.rb
@@ -10,32 +10,39 @@ class Card
 end
 
 class Deck
-    def self.generate_deck(type_suits, type_values)
+    def self.generate_shuffled_deck()
         deck = []
+        type_suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+        type_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
         type_suits.each do |suit|
             type_values.each do |value|
                 deck << [suit, value]
             end
         end
-        return deck
-    end
-
-    def self.shuffle_deck(deck)
         return deck.shuffle
     end
 
-    def self.deal_card(deck, player, amount)
-        for index in [0, amount - 1]
-            card = Card.identify(deck, index)
-            return card
-        end
+
+    def self.deal_card(deck)
+        card = Card.identify(deck, 0)
+        return card
     end
 
-    
 end
 
 class Hand
-    #something
+    def self.create_hand(amount, players)
+        hand = []
+        count = 0
+        shuffled_deck = Deck.generate_shuffled_deck()
+        while count < amount
+            next_card = Deck.deal_card(shuffled_deck)
+            shuffled_deck.delete next_card
+            hand << next_card
+            count += 1
+        end
+        return hand
+    end
 end
 
 class Player
@@ -43,19 +50,18 @@ class Player
 end
 
 class Game
-    #something
+    def self.start_game()
+        hand = Hand.create_hand(5, 5)
+        puts "Player receives a new hand containing:"
+        card_index = 0
+        for card in hand
+            puts "#{hand[card_index][1]} of #{hand[card_index][0]}"
+            card_index += 1
+        end
+    end
 end
 
-class Pot
-    #something
-end
 
-num_cards_in_deck = 52
-type_suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-type_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
-deck = Deck.generate_deck(type_suits, type_values)
-shuffled_deck = Deck.shuffle_deck(deck)
-player = nil
-card = Deck.deal_card(shuffled_deck, player, 5)
-print("Player receives ", card[1], " of ", card[0])
+
+
